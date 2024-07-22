0746e55466bf21d666f9cfda75bfc35dfc5e1089
Lucas Kim <lucaskamadakim@gmail.com>
2024-07-21 22:02:41 -0400

    Implement home page

---
+++ src/utils/page-info.ts
@@ -0,0 +1,21 @@
+export const navigation = [
+  {
+    name: "Experience",
+    href: "/experience",
+  },
+  {
+    name: "Education",
+    href: "/education",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+];
+
+export const colorSchemes: { [id: string]: string } = {
+  "/": "bg-black text-white",
+  "/experience": "bg-orange-200 text-black",
+  "/education": "bg-purple-200 text-black",
+  "/projects": "bg-green-200 text-black",
+};
