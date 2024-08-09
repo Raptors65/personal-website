97203b8fea822c4099886ca673e8a644ecb176cb
Lucas Kim <lucaskamadakim@gmail.com>
2024-08-09 09:26:36 -0400

    Add dreamland buddies project

--- src/utils/page-info.ts
+++ src/utils/page-info.ts
@@ -16,6 +16,8 @@ export const navigation = [
 export const colorSchemes: { [id: string]: string } = {
   "/": "bg-black text-white",
   "/experience": "bg-orange-200 text-black",
+  "/experience/work": "bg-yellow-200 text-black",
+  "/experience/extracurriculars": "bg-red-200 text-black",
   "/education": "bg-purple-200 text-black",
   "/projects": "bg-green-200 text-black",
 };
