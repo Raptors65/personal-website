ef837fe77337764518073ec7c9bd308f74606e0e
Lucas Kim <lucaskamadakim@gmail.com>
2024-08-10 09:55:53 -0400

    Fix pathnames bug

--- src/utils/page-info.ts
+++ src/utils/page-info.ts
@@ -1,23 +1,23 @@
 export const navigation = [
   {
     name: "Experience",
-    href: "/experience",
+    href: "/experience/",
   },
   {
     name: "Education",
-    href: "/education",
+    href: "/education/",
   },
   {
     name: "Projects",
-    href: "/projects",
+    href: "/projects/",
   },
 ];
 
 export const colorSchemes: { [id: string]: string } = {
   "/": "bg-black text-white",
-  "/experience": "bg-orange-200 text-black",
-  "/experience/work": "bg-yellow-200 text-black",
-  "/experience/extracurriculars": "bg-red-200 text-black",
-  "/education": "bg-purple-200 text-black",
-  "/projects": "bg-green-200 text-black",
+  "/experience/": "bg-orange-200 text-black",
+  "/experience/work/": "bg-yellow-200 text-black",
+  "/experience/extracurriculars/": "bg-red-200 text-black",
+  "/education/": "bg-purple-200 text-black",
+  "/projects/": "bg-green-200 text-black",
 };
